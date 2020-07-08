d760ce74aac8e36ac968ee1a00beb81315fec16d
sarwar_sikder <amin.se.aiub@gmail.com>
2020-07-08 19:35:52 +0600

    famework basic structure development

--- index.php
+++ index.php
@@ -2,19 +2,5 @@
 	//var_dump($GLOBALS);
 	define('APP_ROOT', "$_SERVER[DOCUMENT_ROOT]/myapp");
 
-/*	$hasError=true;
-	if(count($_GET)>0){
-		$key = array_keys($_GET)[0]; //ex: person_add
-		$path = explode("_", $key);
-		if(count($path)==2){
-			$hasError = false;
-			$controller = $path[0]; //ex: person
-			$view = $path[1]; //ex: add
-			$isDispatchedByFrontController=true;
-			include_once(APP_ROOT."/app/controller/".$controller."_controller.php");				
-		}
-	}
-	if($hasError){
-		include_once(APP_ROOT."/app/error.php");
-	}*/
+
 ?>
